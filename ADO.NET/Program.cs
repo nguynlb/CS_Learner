7900621c0a3532b53bb4713316a1b5d48152e5df
Long Bảo <114165590+LongBaoCoder2@users.noreply.github.com>
2023-05-02 11:19:25 +0700

    Learn more about ADO.NET & Initial TableView App Project

--- ADO.NET/Program.cs
+++ ADO.NET/Program.cs
@@ -1,8 +1,30 @@
-﻿using System.Data.Common;
+﻿using System.Net;
+using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
 namespace Program
 {
     class Program{
+        public static void ShowDataTable(DataTable dt) {
+            
+            Console.WriteLine($"TableName : {dt.TableName}");
+
+            foreach (DataColumn col in dt.Columns){
+                Console.Write($"{col.ColumnName, 20}");
+            }
+
+            Console.WriteLine();
+            int noColumn = dt.Columns.Count;
+
+            foreach (DataRow row in dt.Rows){
+                for (int i = 0; i < noColumn; i++){
+                    Console.Write($"{row[i], 20}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
         public static void Main() {
 
             // StringBuild
@@ -13,7 +35,7 @@ namespace Program
             sqlStringBuild["PWD"] = "Password123";
 
             string sqlString = sqlStringBuild.ToString();
-            Console.WriteLine(sqlString);
+            // Console.WriteLine(sqlString);
 
             using var sqlConnection = new SqlConnection(sqlString);
 
@@ -24,15 +46,56 @@ namespace Program
             using DbCommand command = new SqlCommand();
             command.Connection = sqlConnection;
             /*   Connection
-                 Command     ----->   ListCommand */
+                 Command     ----->   ListCommand => Data Adapter */
             
             // Sql Text
             // command.CommandText = "SQL";
             command.CommandText = "SELECT * FROM Sanpham WHERE Gia > 100000.00";
-            var dataReader = command.ExecuteReader();
-            while (dataReader.Read()){
-                Console.WriteLine($"{dataReader["TenSanPham"], 50} co gia la : {dataReader["Gia"], 10}");
-            }
+            // using var dataReader = command.ExecuteReader(); 
+            // while (dataReader.Read()){
+            //     Console.WriteLine($"{dataReader["TenSanPham"], 50} co gia la : {dataReader["Gia"], 10}");
+            // }
+
+
+            // //  Command ---> DataTable => DataSet
+            // var dataSet = new DataSet();
+            // // DataTables => Data Collection => DataSet (Local)
+            // // DataStore (DB)
+            // var sanPhamTable = new DataTable("SanPham");
+            // dataSet.Tables.Add(sanPhamTable);
+            // sanPhamTable.Columns.Add("ID");
+            // sanPhamTable.Columns.Add("Ten");
+            // sanPhamTable.Columns.Add("Gia");
+
+            // sanPhamTable.Rows.Add(1, "Tach tra", 12);
+            // sanPhamTable.Rows.Add(2, "Am chen", 10);
+            // sanPhamTable.Rows.Add(3, "Cay mini", 8);
+
+            // ShowDataTable(sanPhamTable);
+
+
+            // DataAdapter
+            var dataAdapter = new SqlDataAdapter();
+            dataAdapter.TableMappings.Add("Table", "SanPham");
+            dataAdapter.SelectCommand = new SqlCommand("SELECT SanphamID, Gia FROM Sanpham", sqlConnection);
+            // InsertCommand
+            dataAdapter.InsertCommand = new SqlCommand("insert into Sanpham (SanphamID, Gia) values (@SanphamID, @Gia)", sqlConnection);
+            dataAdapter.InsertCommand.Parameters.Add("@Gia", SqlDbType.Int, 13, "Gia");
+            dataAdapter.InsertCommand.Parameters.Add("@SanphamID", SqlDbType.Decimal, 13, "SanphamID");
+
+
+
+            var dataSet = new DataSet();
+            // Đổ dữ liệu từ dataAdapter vào DataSet
+            dataAdapter.Fill(dataSet);
+            DataTable ?table = dataSet.Tables["SanPham"];
+            var row = table?.Rows.Add();
+            row["Gia"] = "280000";
+            row["SanphamID"] = "82";
+            if (table != null) ShowDataTable(table);
+
+            // Update
+            dataAdapter.Update(dataSet);
 
 
             // Close Connection
---
+++ Project_TableView/Script/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Program {
+    class Program {
+
+        static void Main() {
+            // Config Sql String
+            var sqlStringBuilder = new SqlConnectionStringBuilder();
+            sqlStringBuilder.Add("Database", "xtlab");
+            sqlStringBuilder.Add("UID", "sa");
+            sqlStringBuilder.Add("PWD", "Password123");
+            sqlStringBuilder.Add("Server", "localhost,1433");
+
+            // Connect Object 
+            using var connection = new SqlConnection(sqlStringBuilder.ToString());
+
+            // Open Connection
+            connection.Open();
+
+            Console.WriteLine(connection);
+            // Close Connection
+            connection.Close();
+        }
+    }
+}
